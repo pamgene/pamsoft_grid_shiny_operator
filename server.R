928a1bffa027d216634bb559a4a42716356afe5e
Thiago <tsmonteiro@gmail.com>
2021-10-21 15:23:08 +0000

    Check grid var

--- server.R
+++ server.R
@@ -271,22 +271,24 @@ shinyServer(function(input, output, session) {
   
   props     <- reactive({get_operator_props(getCtx(session), imgInfo()[1])})
   
-#  observe({
-#
-#    spotPitch <- as.numeric(props()$grdSpotPitch)
-#    spotSize  <- as.numeric(props()$grdSpotSize)
-#  
-#    
-#    off <- (spotPitch * spotSize)/2
-#    
-#    x <- grid$X()
-#    y <- grid$Y()
-#    r <- rep( off, length(x) )
-#    
-#    df <- data.frame(x=x, y=y, radius=r)
-#    session$sendCustomMessage("imageDisplay", df)
-#
-#  })
+  observe({
+
+    if( !is.null(grid$X())){
+      spotPitch <- as.numeric(props()$grdSpotPitch)
+      spotSize  <- as.numeric(props()$grdSpotSize)
+    
+      
+      off <- (spotPitch * spotSize)/2
+      
+      x <- grid$X()
+      y <- grid$Y()
+      r <- rep( off, length(x) )
+      
+      df <- data.frame(x=x, y=y, radius=r)
+      session$sendCustomMessage("imageDisplay", df)
+    }
+
+  })
   
   
   observeEvent(input$gridOverlay, {
